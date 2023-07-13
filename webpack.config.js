a5cc1fc904cf106b0b9210274eb980594d064cb0
Seongbin Kim <seongbin9786@gmail.com>
2023-07-13 22:49:00 +0900

    chore: compress 옵션 활성화 및 Webpack-bundle-analyzer 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/index.tsx',
@@ -25,7 +26,8 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    process.NODE_ENV === "production" ? false : new Dotenv()
+    process.NODE_ENV === "production" ? false : new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ].filter(Boolean),
   module: {
     rules: [
